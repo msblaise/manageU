2c82cf1e24719caa0404b01bb8628ce336122f1b
msblaise <gblai004@fiu.edu>
2020-01-25 21:28:13 -0500

    Created activities for empty tasks and tasks pages, started empty layout

--- app/src/main/java/com/example/manageu/TaskActivity.kt
+++ app/src/main/java/com/example/manageu/TaskActivity.kt
@@ -3,7 +3,7 @@ package com.example.manageu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
