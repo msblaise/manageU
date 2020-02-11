388bcf81a19ce00b6a8f14fa058f6b37e63d197e
Dayana Gil <dayi9798@gmail.com>
2020-02-11 16:22:24 -0500

    Added login/registration activities

---
+++ app/src/main/java/com/example/manageu/RegistrationLoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.manageu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrationLoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_registration)
+    }
+}
