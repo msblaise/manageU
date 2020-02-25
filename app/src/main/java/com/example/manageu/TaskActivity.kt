199d22e18fe0b85155d12058941647c958ca5b6d
msblaise <gblai004@fiu.edu>
2020-02-25 18:11:49 -0500

    Removed some files

--- app/src/main/java/com/example/manageu/TaskActivity.kt
+++ app/src/main/java/com/example/manageu/TaskActivity.kt
@@ -7,6 +7,6 @@ class TaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_task)
     }
 }
