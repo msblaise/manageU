5636e0ac85a70dfe15269a4a41f1660eea0ac999
msblaise <gblai004@fiu.edu>
2020-02-26 16:01:26 -0500

    Created bottom navigation

---
+++ app/src/main/java/com/example/manageu/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.manageu
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity(), DashboardFragment.OnFragmentInteractionListener, TaskFragment.OnFragmentInteractionListener, StatsFragment.OnFragmentInteractionListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
