79bcb36636a17adfd9f16106a89092dccb91f38a
msblaise <gblai004@fiu.edu>
2020-06-22 20:51:02 -0400

    Fixed bottom nav frag state bug

--- app/src/main/java/com/example/manageu/MainActivity.kt
+++ app/src/main/java/com/example/manageu/MainActivity.kt
@@ -3,25 +3,40 @@ package com.example.manageu
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.Fragment
+import com.example.manageu.fragments.DashboardFragment
+import com.example.manageu.fragments.StatsFragment
+import com.example.manageu.fragments.TaskFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), DashboardFragment.OnFragmentInteractionListener, TaskFragment.OnFragmentInteractionListener, StatsFragment.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // set up bottom nav with nav controller (nav host)
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        NavigationUI.setupWithNavController(bottomNavigation, navController)
+        val dashFragment = DashboardFragment()
+        val statsFragment = StatsFragment()
+        val taskFragment = TaskFragment()
+
+        makeCurrentFragment(dashFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_dash -> makeCurrentFragment(dashFragment)
+                R.id.ic_stats -> makeCurrentFragment(statsFragment)
+                R.id.ic_tasks -> makeCurrentFragment(taskFragment)
+            }
+            true
+        }
+
     }
 
-    // override from the fragment classes
-    override fun onFragmentInteraction(uri: Uri) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
 }
